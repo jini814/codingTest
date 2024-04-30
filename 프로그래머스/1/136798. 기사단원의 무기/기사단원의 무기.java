0903b4b90bc936376f819b9dc27e56473fab691a
yujin_ <129928574+jini814@users.noreply.github.com>
2024-04-30 18:01:11 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.78 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -7,12 +7,13 @@ class Solution {
         for(int i = 1; i <= number; i++){
             int count = 0;
             for(int j = 1; j * j <= i; j++){
-                if( j * j == i) {
-                    count++;
-                } else if (i % j == 0) {
-                    count += 2;
-                }
-            }
+    if( j * j == i) {
+        count++;
+    } else if (i % j == 0) {
+        count += 2;
+    }
+}
+
             if (count > limit) {
                 count = power;
             }
