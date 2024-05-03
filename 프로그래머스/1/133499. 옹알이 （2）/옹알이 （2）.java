096e5738b72c42ebac2ba3b353cd594e6f67ae71
yujin_ <129928574+jini814@users.noreply.github.com>
2024-05-03 17:23:07 +0900

    [level 1] Title: 옹알이 (2), Time: 5.78 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] babbling) {
+        String[] str = {"aya", "ye", "woo", "ma"};
+        String[] str2 = {"ayaaya", "yeye", "woowoo", "mama"};
+        int answer = 0;
+        
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < str.length; j++) {
+                babbling[i] = babbling[i].replaceAll(str2[j], "1").replaceAll(str[j], " ");
+            }
+            
+            if (babbling[i].trim().length() == 0) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
