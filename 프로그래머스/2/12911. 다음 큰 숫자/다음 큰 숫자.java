5387d6ac001acee9d477e8ba5c0f69f2128dbd29
yujin_ <129928574+jini814@users.noreply.github.com>
2024-06-10 09:18:42 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,12 +1,9 @@
 class Solution {
     public int solution(int n) {
-        //n 이진수에서 1 갯수만 세기
-        int num = Integer.bitCount(n);
-        //n+1부터 시작해서 찾기
-        int nextNum = n + 1;
+        int num = Integer.bitCount(n); //n 이진수에서 1 갯수만 세기
+        int nextNum = n + 1; //n+1부터 시작해서 찾기
         
-        while (true) {
-            //n+1 이진수 1 갯수랑 n이랑 같은지 비교
+        while (true) { //n+1 이진수 1 갯수랑 n이랑 같은지 비교
             if (Integer.bitCount(nextNum) == num) {
                 return nextNum;
             }
