4bd994ad5e10027383358948d4d06d6606c237d7
yujin_ <129928574+jini814@users.noreply.github.com>
2024-07-03 09:25:06 +0900

    [level 2] Title: H-Index, Time: 0.34 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        
+        int n = citations.length;
+        for (int i = 0; i < n; i++) {
+            int hIndex = n - i;
+            if (citations[i] >= hIndex) {
+                return hIndex;
+            }
+        }
+        return 0;
+    }
+}
