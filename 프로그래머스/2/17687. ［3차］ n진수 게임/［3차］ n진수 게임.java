d70b6e2cf4e559eb20afb1794920c19b0aafac0e
yujin_ <129928574+jini814@users.noreply.github.com>
2024-09-13 22:45:38 +0900

    [level 2] Title: [3차] n진수 게임, Time: 12.93 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,16 +1,16 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder answer = new StringBuilder();
-        StringBuilder allNumbers = new StringBuilder();
+        StringBuilder numbers = new StringBuilder();
         int num = 0;
 
-        while (allNumbers.length() < t * m) {
-            allNumbers.append(Integer.toString(num, n).toUpperCase());
+        while (numbers.length() < t * m) {
+            numbers.append(Integer.toString(num, n).toUpperCase());
             num++;
         }
 
         for (int i = p - 1; i < t * m; i += m) {
-            answer.append(allNumbers.charAt(i));
+            answer.append(numbers.charAt(i));
         }
 
         return answer.toString();
