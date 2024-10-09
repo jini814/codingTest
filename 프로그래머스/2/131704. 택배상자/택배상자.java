4a9d0e53d4a896a6102c7153dc158fc5f0f87257
yujin_ <129928574+jini814@users.noreply.github.com>
2024-10-09 21:31:22 +0900

    [level 2] Title: 택배상자, Time: 67.24 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,36 +3,32 @@ import java.util.*;
 class Solution {
     public int solution(int[] order) {
         Stack<Integer> stack = new Stack<>();
-        int index = 0;  // order 배열에서 현재 실어야 하는 상자의 위치
-        int boxNumber = 1;  // 현재 컨테이너 벨트에서 처리해야 하는 상자의 번호
-        int loadedBoxes = 0;  // 트럭에 실은 상자의 수
+        int index = 0;  
+        int boxNumber = 1;  
+        int loadedBoxes = 0;  
         
         while (boxNumber <= order.length) {
             if (boxNumber == order[index]) {
-                // 현재 컨테이너 벨트의 상자를 바로 트럭에 실을 수 있는 경우
                 loadedBoxes++;
                 index++;
                 boxNumber++;
             } else if (!stack.isEmpty() && stack.peek() == order[index]) {
-                // 보조 컨테이너 벨트에서 실어야 하는 상자가 있는 경우
                 stack.pop();
                 loadedBoxes++;
                 index++;
             } else {
-                // 현재 상자를 보조 컨테이너 벨트에 보관
                 stack.push(boxNumber);
                 boxNumber++;
             }
         }
         
-        // 컨테이너 벨트에서 모든 상자를 처리한 후에도, 스택에 남은 상자를 처리
         while (!stack.isEmpty()) {
             if (stack.peek() == order[index]) {
                 stack.pop();
                 loadedBoxes++;
                 index++;
             } else {
-                break;  // 더 이상 실을 수 없으면 중단
+                break; 
             }
         }
 
